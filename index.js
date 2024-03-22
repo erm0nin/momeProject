288afea6aba3b14e5d1573ba46022957dbb6bfb1
dima <ermonindima0@gmail.com>
2024-03-22 19:49:00 +0300

    one

---
+++ index.js
@@ -0,0 +1,27 @@
+// let person = {
+//   age : 18,
+//   name : "<dima>",
+//   address : "123 Main St",  
+
+// }
+
+
+// console.log(person['name']);
+
+let arr = ['orange.png', 'apple.png', 'banana.jpeg', 'kiwi.png'];
+
+
+
+function array() {
+  let last = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes('.png')) {
+      last.push(arr[i]);
+    }
+  }
+  return last;
+}
+
+
+console.log(array(arr));
+
